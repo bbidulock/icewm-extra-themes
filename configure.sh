739996fc391a9d39be8dc14ce96151fe5f2b4489
Brian Bidulock <bidulock@openss7.org>
2019-09-10 00:22:50 -0600

    better defaults now

--- configure.sh
+++ configure.sh
@@ -8,18 +8,18 @@
 
 case "`uname -m`" in
 	i686)
-		CPPFLAGS="-D_FORTIFY_SOURCE"
-		CFLAGS="-march=i686 -mtune=generic -O2 -pipe -fstack-protector-strong --param=ssp-buffer-size=4"
-		CXXFLAGS="-march=i686 -mtune=generic -O2 -pipe -fstack-protector-strong --param=ssp-buffer-size=4"
-		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro"
+		CPPFLAGS="-D_FORTIFY_SOURCE=2"
+		CFLAGS="-march=i686 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
+		CXXFLAGS="-march=i686 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
+		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
 		DEBUG_CFLAGS="-g -ggdb -fvar-tracking-assignments"
 		DEBUG_CXXFLAGS="-g -ggdb -fvar-tracking-assignments"
 	;;
 	x86_64)
 		CPPFLAGS="-D_FORTIFY_SOURCE=2"
-		CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fstack-protector-strong --param=ssp-buffer-size=4"
-		CXXFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fstack-protector-strong --param=ssp-buffer-size=4"
-		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro"
+		CFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
+		CXXFLAGS="-march=x86-64 -mtune=generic -O2 -pipe -fstack-protector-strong -fno-plt"
+		LDFLAGS="-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now"
 		DEBUG_CFLAGS="-g -ggdb -fvar-tracking-assignments"
 		DEBUG_CXXFLAGS="-g -ggdb -fvar-tracking-assignments"
 	;;
@@ -27,11 +27,14 @@ esac
 
 ./configure \
 	--enable-maintainer-mode \
-	--prefix=/usr \
-	--sysconfdir=/etc \
+	--enable-dependency-tracking \
 	CPPFLAGS="$CPPFLAGS" \
 	CFLAGS="$DEBUG_CFLAGS -Wall -Werror $CFLAGS" \
 	CXXFLAGS="$DEBUG_CXXFLAGS -Wall -Werror $CXXFLAGS" \
 	LDFLAGS="$LDFLAGS" \
 	DEBUG_CFLAGS="$DEBUG_CFLAGS" \
 	DEBUG_CXXFLAGS="$DEBUG_CXXFLAGS"
+
+# cscope target won't work without this
+#
+[ -f po/Makefile ] && echo -e '\n%:\n\t@:\n\n' >> po/Makefile
